4ccd5180f7b8b3fd5a640ef2e08ca84645f2eeb4
Tanay Anand <tanayanand98@gmail.com>
2017-07-08 22:37:55 +0530

    Final Project

--- Classifier.py
+++ Classifier.py
@@ -11,14 +11,17 @@ features_train, features_test, labels_train, labels_test = Vectorizer()
 features_new = NVectorizer()
 
 from sklearn import svm
-clf = svm.SVC(kernel = 'rbf', C = 100000.0)
+clf = svm.SVC(C=1, kernel='linear')
+
 
 t0 = time()
 clf.fit(features_train, labels_train)
 print "Training time:", round(time()-t0, 6), "s"
 
 from sklearn.metrics import accuracy_score
-pred = clf.predict(features_test)
-print "Accuracy = ", accuracy_score(labels_test, pred)
+pred1 = clf.predict(features_train)
+pred2 = clf.predict(features_test)
+print "Training Accuracy = ", accuracy_score(labels_train, pred1)
+print "Testing Accuracy = ", accuracy_score(labels_test,pred2)
 
 print "Category of new mail : ", clf.predict(features_new)
